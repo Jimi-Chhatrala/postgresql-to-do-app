937e264d208b34dabe00bb96ec1240b362ae362b
jimi-chhatrala <jimichhatrala8488@gmail.com>
2024-08-31 23:59:10 +0530

    PostgreSQL To Do App - Readme Updated

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# postgresql-to-do-app
+# PostgreSQL To Do App
+
+The app is design in a way that the user can Sign In and Manage their todos.
--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,5 +1,42 @@
 const Modal = () => {
-  return <div>Modal</div>;
+  const mode = "edit";
+
+  const handleChange = () => {
+    console.log("changing!");
+  };
+
+  return (
+    <div className="overlay">
+      <div className="modal">
+        <div className="form-title-container">
+          <h3>Let's {mode} you task</h3>
+          <button>X</button>
+        </div>
+        <form>
+          <input
+            type="text"
+            maxLength={30}
+            placeholder=" Your task goes here"
+            name="title"
+            value={""}
+            onChange={handleChange}
+            required
+          />
+          <br />
+          <input
+            type="range"
+            min="0"
+            max="100"
+            name="progress"
+            value={""}
+            onChange={handleChange}
+            required
+          />
+          <input type="submit" value="" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
